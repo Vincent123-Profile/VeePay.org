0a301e91066f7695abb37e2d80d29448cd2cb01d
agboola vincent <agboolavincent67@gmail.com>
2024-04-28 18:45:39 +0100

    c

--- forgotten.js
+++ forgotten.js
@@ -70,13 +70,13 @@
   <script>
     // Your Firebase configuration
     const firebaseConfig = {
-       apiKey: "AIzaSyDObCfvc9Cf1BpR_P6y2loBWNhSUPgorqg",
-       authDomain: "veepay-4521d.firebaseapp.com",
-       projectId: "veepay-4521d",
-       storageBucket: "veepay-4521d.appspot.com",
-       messagingSenderId: "56228740854",
-       appId: "1:56228740854:web:1498028cdfdb533b3536d7",
-       measurementId: "G-LVLJ81JTMX"
+      apiKey: "AIzaSyDObCfvc9Cf1BpR_P6y2loBWNhSUPgorqg",
+      authDomain: "veepay-4521d.firebaseapp.com",
+      projectId: "veepay-4521d",
+      storageBucket: "veepay-4521d.appspot.com",
+      messagingSenderId: "56228740854",
+      appId: "1:56228740854:web:1498028cdfdb533b3536d7",
+      measurementId: "G-LVLJ81JTMX"
     };
 
     // Initialize Firebase
@@ -93,12 +93,14 @@
       auth.sendPasswordResetEmail(email)
         .then(() => {
           // Password reset email sent successfully
+          console.log("Password reset link sent. Please check your email.");
           alert("Password reset link sent. Please check your email.");
           // Redirect to sign-in page
-          window.location.href = "sign in.html";
+          window.location.href = "signin.html";
         })
         .catch((error) => {
           // Handle errors
+          console.error("Error sending password reset email:", error);
           alert(error.message);
         });
     });
