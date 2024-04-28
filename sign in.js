b02cce2a51d67df098c5a77b5429f504f40d6718
agboola vincent <agboolavincent67@gmail.com>
2024-04-28 16:27:01 +0100

    sign in base

--- sign in.js
+++ sign in.js
@@ -21,39 +21,37 @@ const firebaseConfig = {
     appId: "1:56228740854:web:1498028cdfdb533b3536d7",
     measurementId: "G-LVLJ81JTMX"
   };
-  
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Initialize variables
-  const auth = firebase.auth();
-  
-  // Set up the sign-in function
-  function signIn() {
-    // Get the user's email and password
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
-    // Sign in the user with Firebase Authentication
-    auth.signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in successfully
-        const user = userCredential.user;
-        console.log("User signed in:", user);
-        // You can redirect the user to another page or perform any other action here
-      })
-      .catch((error) => {
-        // An error occurred
-        const errorMessage = error.message;
-        showError(errorMessage);
-      });
-  }
-  
-  // Function to display error messages
-  function showError(message) {
-    const errorDetail = document.getElementById('error-detail');
-    errorDetail.textContent = message;
-    const errorMessage = document.getElementById('error-message');
-    errorMessage.style.display = 'block';
-  }
-  
+firebase.initializeApp(firebaseConfig);
+
+// Initialize variables
+const auth = firebase.auth();
+
+// Set up the sign-in function
+function signIn() {
+  // Get the user's email/phone and password
+  const emailOrPhone = document.getElementById('phoneOrEmail').value;
+  const password = document.getElementById('password').value;
+
+  // Sign in the user with Firebase Authentication
+  auth.signInWithEmailAndPassword(emailOrPhone, password)
+    .then((userCredential) => {
+      // Signed in successfully
+      const user = userCredential.user;
+      console.log("User signed in:", user);
+      // You can redirect the user to another page or perform any other action here
+    })
+    .catch((error) => {
+      // An error occurred
+      const errorMessage = error.message;
+      showError(errorMessage);
+    });
+}
+
+// Function to display error messages
+function showError(message) {
+  const errorDetail = document.getElementById('error-detail');
+  errorDetail.textContent = message;
+  const errorMessage = document.getElementById('error-message');
+  errorMessage.style.display = 'block';
+}
